3179e72ffd4dd418d92cbb44cbf4bcde85d56185
areta1921 <agetahun567@gmail.com>
2022-03-04 23:27:51 -0700

    change

--- src/test/java/org/generation/ItemsAPI/ItemServiceMySQLTest.java
+++ src/test/java/org/generation/ItemsAPI/ItemServiceMySQLTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mock;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
